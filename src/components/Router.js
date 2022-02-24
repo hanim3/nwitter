88f6896c49d02cc5d8324cceb92a4c5f599749d3
hanim3 <hanim0308as12@naver.com>
2022-02-24 20:45:13 +0900

    Nwitter Authentication

--- src/components/Router.js
+++ src/components/Router.js
@@ -4,18 +4,24 @@ import {
   Route,
   Switch
 } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState();
+const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {/* isLoggedIn = true면 Navigation 띄운다. */}
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
               <Home />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
             </Route>
           </>
         ) : (
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,76 @@
-import React from "react";
+import { authService, firebaseInstance } from "fbase";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [form, setForm] = useState({ email: "", password: "" });
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
+  const onChange = (event) => {
+    const { name, value } = event.target;
+    setForm({ ...form, [name]: value });
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await authService.createUserWithEmailAndPassword(
+          form['email'], 
+          form['password']
+        );
+      } else {
+        data = await authService.signInWithEmailAndPassword(
+          form['email'], 
+          form['password']
+        );
+      }
+    } catch(error) {
+      setError(error.message);
+    }
+  };
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (event) => {
+    const { name } = event.target;
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if(name === "github") {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
+  }
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input 
+          name="email" 
+          type="text" 
+          placeholder="Email" 
+          required 
+          value={form['email']}
+          onChange={onChange}
+        />
+        <input 
+          name="password" 
+          type="password" 
+          placeholder="Password" 
+          required 
+          value={form['password']}
+          onChange={onChange}
+        />
+        <input type="submit" value={newAccount ? "Create Account" : "Sign In"} />
+        {error}
+      </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign In" : "Create Account"}
+      </span>
+      <div>
+        <button name="google" onClick={onSocialClick}>Continue with Google</button>
+        <button name="github" onClick={onSocialClick}>Continue with Github</button>
+      </div>
+    </div>
+  );
+}
+
 export default Auth;
